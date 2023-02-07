bd5943d690d894a87270bb526b439664dee803f6
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-02-07 23:04:01 +0200

    modified

--- BuilderInheritance/BuilderInheritance/Program.cs
+++ BuilderInheritance/BuilderInheritance/Program.cs
@@ -1,10 +1,59 @@
 ﻿namespace BuilderInheritance
 {
+    public class Person
+    {
+        public string Name;
+        public string Position;
+
+
+        public class Builder : PersonJobBuilder<Builder>
+        {
+            internal Builder()
+            {
+            }
+        }
+        public static Builder New => new Builder();
+    }
+
+    public abstract class PersonBuilder
+    {
+        protected Person person;
+
+        public Person Build()
+        {
+            return person;
+        }
+    }
+
+    public class PersonInfoBuilder<SELF> : PersonBuilder
+        where SELF :PersonInfoBuilder<SELF>
+    {
+        public SELF Called(string name)
+        {
+            person.Name = name;
+            return (SELF) this;
+        }
+    }
+
+    public class PersonJobBuilder<SELF>
+        : PersonInfoBuilder<SELF>
+        where SELF : PersonJobBuilder<SELF>
+    {
+        public SELF WorksAs(string position)
+        {
+            person.Position = position;
+            return (SELF) this;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var me = Person.New
+                .Called("Vitaliy")
+                .WorksAs("Engineer")
+                .Build();
         }
     }
 }
