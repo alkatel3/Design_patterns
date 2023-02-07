e9b179708a49461f3c5709b1ce72690b648d9574
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-02-07 23:01:54 +0200

    Using constructor imitation with recursive generics

---
+++ BuilderInheritance/BuilderInheritance/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BuilderInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
