e5bd6835b85a096bce55af84907ab9c527d753cc
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-05-05 00:17:15 +0300

    Created Point Factory

---
+++ Factory/Factory/Factory/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace Factory
+{
+    public class Point
+    {
+        private double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // polar 
+        // It's bad you can't do that
+        // public Point(double rho, double theta)
+        // {
+        //   
+        // }
+
+        public Point(double a,
+            double b, // names do not communicate intent
+             CoordinateSystem cs = CoordinateSystem.Cartesian)
+        {
+            switch (cs)
+            {
+                case CoordinateSystem.Polar:
+                    x = a * Math.Cos(b);
+                    y = a * Math.Sin(b);
+                    break;
+                default:
+                    x = a;
+                    y = b;
+                    break;
+            }
+
+            // steps to add a new system
+            // 1. augment CoordinateSystem
+            // 2. change ctor
+        }
+
+        // factory property
+        public static Point Origin => new Point(0, 0);
+
+        // constant field
+        public static Point Origin2 = new Point(0, 0);
+
+        // factory method
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+        public enum CoordinateSystem
+        {
+            Cartesian,
+            Polar
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+
+        // make it lazy
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+        }
+    }
+
+    public class PointFactory
+    {
+        public static Point NewCartesianPoint
+            (double x, double y)
+        {
+            return new Point(x, y);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Point(2, 3, Point.CoordinateSystem.Cartesian);
+            var origin = Point.Origin;
+
+            var p2 = Point.Factory.NewCartesianPoint(1, 2);
+        }
+    }
+}
