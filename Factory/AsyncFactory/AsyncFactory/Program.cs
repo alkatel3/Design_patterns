9cd318c5eb3f4bbda53bf80dc44c6f0b1aca2c91
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-05-05 01:03:20 +0300

    Created async factory

---
+++ Factory/AsyncFactory/AsyncFactory/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace AsyncFactory
+{
+    public interface IAsyncItit<T>
+    {
+        Task<T> InitAsync();
+    }
+
+    public class Foo:IAsyncItit<Foo>
+    {
+        /*public*/ public Foo()
+        {
+            //await Task.Delay(1000);
+        }
+
+        // ↓↓
+        public async Task<Foo> InitAsync()
+        {
+            // some work here
+            await Task.Delay(1000);
+            return this; // fluent
+        }
+
+        public static async Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return await result.InitAsync();
+        }
+    }
+
+    public static class AsyncFactory
+    {
+        public static async Task<T> Create<T>()
+            where T : IAsyncItit<T>, new()
+        {
+            return await new T().InitAsync();
+        }
+    }
+
+    public class Program
+    {
+        public static async void Main(string[] args)
+        {
+            // var foo = new Foo();
+            // await foo.InitAsync();
+
+            // instead
+            //var foo = await Foo.CreateAsync();
+
+            // or
+            var foo2 = AsyncFactory.Create<Foo>();
+        }
+    }
+}
