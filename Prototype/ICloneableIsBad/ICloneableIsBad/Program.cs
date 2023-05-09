a9d1350c085b0d4cb68fa9997ec23965f344b498
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-05-09 13:55:06 +0300

    Implemented program ICloneableIsBat like a bat way to do pattern prototype

---
+++ Prototype/ICloneableIsBad/ICloneableIsBad/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace ICloneableIsBad
+{
+    public class Person: ICloneable
+    {
+        public string[] Names;
+        public Address Address;
+
+        public Person(string[] names, Address address)
+        {
+            Names = names;
+            Address = address;
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+            //return new Person(Names, (Address)Address.Clone());
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(' ', Names)}," +
+                $"{nameof(Address)}: {Address}";
+        }
+    }
+
+    public class Address: ICloneable
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public object Clone()
+        {
+            return new Address(StreetName, HouseNumber);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}," +
+                $" {nameof(HouseNumber)}: {HouseNumber}";
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person(
+              new[] { "John", "Smith" },
+              new Address("London Road", 123));
+
+            var jane = (Person)john.Clone();
+            jane.Address.HouseNumber = 321; // oops, John is now at 321
+
+            // this doesn't work
+            //var jane = john;
+
+            // but clone is typically shallow copy
+            jane.Names[0] = "Jane";
+
+            Console.WriteLine(john);
+            Console.WriteLine(jane);
+
+
+        }
+    }
+}
