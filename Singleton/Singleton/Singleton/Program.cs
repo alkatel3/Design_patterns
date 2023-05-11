37c28ae4dd674a21dea9f1177e423296ee3e1860
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-05-12 00:33:40 +0300

    Created typical singleton

---
+++ Singleton/Singleton/Singleton/Program.cs
@@ -0,0 +1,49 @@
+﻿using MoreLinq;
+
+namespace Singleton
+{
+    public class SingletoDatabase
+    {
+        private Dictionary<string, int> capitals;
+        private static int instanceCount;
+        public static int Count => instanceCount;
+
+        private SingletoDatabase()
+        {
+            capitals = File.ReadAllLines(
+                Path.Combine(
+                    new FileInfo(typeof(SingletoDatabase).Assembly.Location).DirectoryName,
+                    "capitals.txt"
+                    )
+                ).Batch(2).ToDictionary(
+                list => list.ElementAt(0).Trim(),
+                list => int.Parse(list.ElementAt(1)));
+        }
+
+        public int GetPopulation(string ciry)
+        {
+            return capitals[ciry];
+        }
+
+        private static Lazy<SingletoDatabase> instance =
+            new Lazy<SingletoDatabase>(() =>
+            {
+                instanceCount++;
+                return new SingletoDatabase();
+            });
+
+        public static SingletoDatabase Instance = instance.Value;
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var db = SingletoDatabase.Instance;
+
+            var city = "Tokyo";
+
+            Console.WriteLine($"{city} has population {db.GetPopulation(city)}");
+        }
+    }
+}
---
+++ Singleton/Singleton/SingletonTests1/SingletoDatabaseTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Singleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.Tests
+{
+    [TestFixture]
+    public class SingletoDatabaseTests
+    {
+        [Test]
+        public void IsSingleton()
+        {
+            var db = SingletoDatabase.Instance;
+            var db2 = SingletoDatabase.Instance;
+
+            Assert.That(db, Is.SameAs(db2));
+            Assert.That(SingletoDatabase.Count, Is.EqualTo(1));
+        }
+    }
+}
