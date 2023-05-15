17df0432d29052365e15e4a5c22c53cdc8c29f43
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-05-15 16:58:48 +0300

    Implemented pattern Adapter with property surrogate

---
+++ Adapter/PropertySurrogate/PropertySurrogate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+
+namespace PropertySurrogate
+{
+    public class CountryStats
+    {
+        [XmlIgnore]
+        public Dictionary<string, string> Capitals { get; set; }
+            = new Dictionary<string, string>();
+        
+        public (string, string)[] CapitalsSerializable
+        {
+            get
+            {
+                return Capitals.Keys.Select(country => 
+                    (country, Capitals[country])).ToArray();
+            }
+            set
+            {
+                Capitals = value.ToDictionary(
+                    x => x.Item1, x => x.Item2);
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stats = new CountryStats();
+            stats.Capitals.Add("France", "Paris");
+
+            var xs = new XmlSerializer(typeof(CountryStats));
+            var sb = new StringBuilder();
+            var sw = new StringWriter(sb);
+            xs.Serialize(sw, stats);
+
+            var newStats = (CountryStats)xs.Deserialize(new StringReader(sb.ToString()));
+
+            Console.WriteLine(newStats?.Capitals["France"]);
+
+        }
+    }
+}
