cef8de1304bb5258a2e36b497b0da2a243c7fc8d
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-05-23 16:24:14 +0300

    Implemented pattern composite in geomentric shapes

---
+++ Composite/GeometricShapes/GeometricShapes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace GeometricShapes
+{
+    public class GraphicObject
+    {
+        public virtual string Name { get; set; } = "Group";
+        public string Color;
+
+        private Lazy<List<GraphicObject>> children = new(()=>new());
+        public List<GraphicObject> Children => children.Value;
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            Print(sb, 0);
+            return sb.ToString();
+        }
+
+        private void Print(StringBuilder sb, int depth)
+        {
+            sb.Append(new string('*', depth))
+                .Append(string.IsNullOrWhiteSpace(Color)?string.Empty : $"{Color}")
+                .AppendLine($"{Name}");
+            foreach (var child in Children)
+                child.Print(sb, depth + 1);
+        }
+    }
+
+    public class Circle : GraphicObject
+    {
+        public override string Name => "Circle";
+    }
+
+    public class Square : GraphicObject
+    {
+        public override string Name => "Square";
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var drawing = new GraphicObject { Name = "My Drawing" };
+            drawing.Children.Add(new Square { Color = "Red" });
+            drawing.Children.Add(new Circle { Color = "Yellow" });
+
+            var group = new GraphicObject();
+            group.Children.Add(new Circle { Color = "Blue" });
+            group.Children.Add(new Square { Color = "Blue" });
+
+            drawing.Children.Add(group);
+
+            Console.WriteLine(drawing);
+        }
+    }
+}
