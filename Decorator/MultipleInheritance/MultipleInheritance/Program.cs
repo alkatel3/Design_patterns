affcfeb2bc6646366161af6d36b041f9b3539e0f
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-05-23 23:44:01 +0300

    Implemented pattern Decorator to do multiple inheritance

---
+++ Decorator/MultipleInheritance/MultipleInheritance/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace MultipleInheritance
+{
+    public interface ICreature
+    {
+        int Age { get; set; }
+    }
+
+    public interface IBird : ICreature
+    {
+        void Fly();
+    }
+
+    public interface ILizard : ICreature
+    {
+        void Crawl();
+    }
+
+    public class Bird : IBird
+    {
+        public int Age { get; set; }
+
+        public void Fly()
+        {
+            if (Age >= 10)
+            {
+                Console.WriteLine("I am flying");
+            }
+        }
+    }
+
+    public class Lizard : ILizard
+    {
+        public int Age { get; set; }
+
+        public void Crawl()
+        {
+            if (Age < 10)
+            {
+                Console.WriteLine("I am crawling");
+            }
+        }
+    }
+
+    public class Dragon : IBird, ILizard
+    {
+        private readonly IBird bird;
+        private readonly ILizard lizard;
+
+        public Dragon(IBird bird, ILizard lizard)
+        {
+            this.bird = bird;
+            this.lizard = lizard;
+        }
+
+        public int Age
+        {
+            get
+            {
+                return bird.Age;
+            }
+            set
+            {
+                bird.Age = value;
+                lizard.Age = value;
+            }
+        }
+        public void Crawl()
+        {
+            lizard.Crawl();
+        }
+
+        public void Fly()
+        {
+            bird.Fly();
+        }
+    }
+}
