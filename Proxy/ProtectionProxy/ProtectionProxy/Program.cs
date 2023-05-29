a88a27ff5f3c9ff5114123ccd4df1d289a0044a7
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-05-29 22:50:47 +0300

    Implemented Pattern proxy particular protection proxy

---
+++ Proxy/ProtectionProxy/ProtectionProxy/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ProtectionProxy
+{
+    public interface ICar
+    {
+        void Drive();
+    }
+
+    public class Car
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car being driven");
+        }
+    }
+
+    public class Driver
+    {
+        public int Age;
+
+        public Driver(int age)
+        {
+            Age = age;
+        }
+    }
+
+    public class CarProxy : ICar
+    {
+        private Car car =new();
+        private Driver driver;
+
+        public CarProxy(Driver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Drive()
+        {
+            if (driver.Age >= 16)
+                car.Drive();
+            else
+                Console.WriteLine("Driver is too young");
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new CarProxy(new Driver(18));
+            car.Drive();
+        }
+    }
+}
