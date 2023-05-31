3c6c0d98f430387d9e97e67ceac07154f5b64837
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-05-31 21:37:43 +0300

    Implemented Pattern Chain of responsibility particular method chain

---
+++ ChainOfResponsibility/MethodChain/MethodChain/Program.cs
@@ -0,0 +1,114 @@
+﻿namespace MethodChain
+{
+    public class Creature
+    {
+        public string Name;
+        public int Attack, Defense;
+
+        public Creature(string name, int attack, int defense)
+        {
+            Name = name;
+            Attack = attack;
+            Defense = defense;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, " +
+                $"{nameof(Attack)}: {Attack}, " +
+                $"{nameof(Defense)}: {Defense}";
+        }
+    }
+
+    public class CreatureModifier
+    {
+        protected Creature creature;
+
+        //singly linked list
+        private CreatureModifier next;
+
+        public CreatureModifier(Creature creature)
+        {
+            this.creature = creature;
+        }
+
+        public void Add(CreatureModifier m)
+        {
+            if (next != null)
+            {
+                next.Add(m);
+            }
+            else
+            {
+                next = m;
+            }
+        }
+
+        public virtual void Handle() => next?.Handle();
+    }
+
+    public class DoubleAttackModifier : CreatureModifier
+    {
+        public DoubleAttackModifier(Creature creature) 
+            : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            Console.WriteLine($"Doubling {creature.Name}'s attack");
+            creature.Attack *= 2;
+            base.Handle();
+        }
+    }
+
+    public class IncreaseDefenseModifier : CreatureModifier
+    {
+        public IncreaseDefenseModifier(Creature creature) 
+            : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            if (creature.Attack <= 2)
+            {
+                Console.WriteLine($"Increasing {creature.Name}'s defense");
+                creature.Defense++;
+            }
+            base.Handle();
+        }
+    }
+
+    public class NoBobuaeaModifier : CreatureModifier
+    {
+        public NoBobuaeaModifier(Creature creature) 
+            : base(creature)
+        {
+        }
+        public override void Handle()
+        {
+            
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var goblin = new Creature("Goblin", 1, 1);
+            Console.WriteLine(goblin);
+
+            var root = new CreatureModifier(goblin);
+
+            //root.Add(new NoBobuaeaModifier(goblin));
+
+            root.Add(new DoubleAttackModifier(goblin));
+            root.Add(new DoubleAttackModifier(goblin));
+            root.Add(new IncreaseDefenseModifier(goblin));
+
+            root.Handle();
+            Console.WriteLine(goblin);
+        }
+    }
+}
