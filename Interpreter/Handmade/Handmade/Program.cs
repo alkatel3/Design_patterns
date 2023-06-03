8c764906aed10f7feabe0ccd45b0f41bce338939
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-06-03 14:26:30 +0300

    Done pattern Interpreter on parsing list of tokins to OOP objekts

--- Interpreter/Handmade/Handmade/Program.cs
+++ Interpreter/Handmade/Handmade/Program.cs
@@ -3,11 +3,53 @@ using System.Text;
 
 namespace Handmade
 {
+    public interface IElement
+    {
+        int Value { get; }
+    }
+
+    public class Integer : IElement
+    {
+        public int Value { get; }
+
+        public Integer(int value)
+        {
+            Value = value;
+        }
+    }
+
+    public class BinaryOperation : IElement
+    {
+        public enum Type
+        {
+            Addition,
+            Subtraction
+        }
+
+        public Type MyType;
+        public IElement Left, Right;
+
+        public int Value
+        {
+            get
+            {
+                switch (MyType)
+                {
+                    case Type.Addition:
+                        return Left.Value + Right.Value;
+                    case Type.Subtraction:
+                        return Left.Value - Right.Value;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
     public class Token
     {
         public enum Type
         {
-            Integer, Plus, Minus, Lparen , Rparen
+            Integer, Plus, Minus, Lparen, Rparen
         }
 
         public Type MyType;
@@ -24,14 +66,14 @@ namespace Handmade
             return $"`{Text}`";
         }
     }
-
+    
     public class Program
     {
         static List<Token> Lex(string input)
         {
             var result = new List<Token>();
 
-            for(int i =0;i<input.Length; i++)
+            for (int i = 0; i < input.Length; i++)
             {
                 switch (input[i])
                 {
@@ -49,7 +91,7 @@ namespace Handmade
                         break;
                     default:
                         var sb = new StringBuilder(input[i].ToString());
-                        for(int j =i+i; j<input.Length; ++j)
+                        for (int j = i + 1; j < input.Length; ++j)
                         {
                             if (char.IsDigit(input[j]))
                             {
@@ -64,17 +106,78 @@ namespace Handmade
                         }
 
                         break;
-
                 }
             }
 
             return result;
         }
+
+        public static IElement Parse(IReadOnlyList<Token> tokens)
+        {
+            var result = new BinaryOperation();
+            bool haveLHS = false;
+
+            for (int i = 0; i < tokens.Count; ++i)
+            {
+                var token = tokens[i];
+
+                switch (token.MyType)
+                {
+                    case Token.Type.Integer:
+                        var integer = new Integer(int.Parse(token.Text));
+                        if (!haveLHS)
+                        {
+                            result.Left = integer;
+                            haveLHS = true;
+                        }
+                        else
+                        {
+                            result.Right = integer;
+                        }
+                        break;
+                    case Token.Type.Plus:
+                        result.MyType = BinaryOperation.Type.Addition;
+                        break;
+                    case Token.Type.Minus:
+                        result.MyType = BinaryOperation.Type.Subtraction;
+                        break;
+                    case Token.Type.Lparen:
+                        int j = i;
+                        for (; j < tokens.Count; ++j)
+                            if (tokens[j].MyType == Token.Type.Rparen)
+                            {
+                                break;
+                            }
+                        var subexpression = tokens.Skip(i + 1).Take(j - i - 1).ToList();
+                        var element = Parse(subexpression);
+                        if (!haveLHS)
+                        {
+                            result.Left = element;
+                            haveLHS = true;
+                        }
+                        else
+                        {
+                            result.Right = element;
+                        }
+
+                        i = j;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
             var input = "(13+4)-(12+1)";
             var tokens = Lex(input);
             Console.WriteLine(string.Join("\t", tokens));
+
+            var parsed = Parse(tokens);
+            Console.WriteLine($"{input} = {parsed.Value}");
         }
+        
     }
 }
