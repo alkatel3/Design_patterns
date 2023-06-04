3749ce9ccba0ad240a218c5b0d9173233b223b76
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-06-04 18:41:54 +0300

    Implemented pattern Iterator based on iterator object

---
+++ Iterator/TreeTraversal/TreeTraversal/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace TreeTraversal
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Left, Right;
+        public Node<T> Parent;
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node(T value, Node<T> left, Node<T> right) : this(value)
+        {
+            Left = left;
+            Right = right;
+
+            Left.Parent = right.Parent = this;
+        }
+    }
+
+    public class InOrderIterator<T>
+    {
+        public Node<T> Current { get; set; }
+        private Node<T> root { get; set; }
+
+        private bool yieldedStart;
+
+        public InOrderIterator(Node<T> root)
+        {
+            this.root = Current = root;
+            while (Current.Left != null)
+            {
+                Current = Current.Left;
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (!yieldedStart)
+            {
+                yieldedStart = true;
+                return true;
+            }
+
+            if (Current.Right != null)
+            {
+                Current = Current.Right;
+                while (Current.Left != null)
+                    Current = Current.Left;
+                return true;
+            }
+            else
+            {
+                var p = Current.Parent;
+                while (p != null && Current == p.Right)
+                {
+                    Current = p;
+                    p = p.Parent;
+                }
+                Current = p;
+                return Current != null;
+            }
+        }
+
+        public void Reset()
+        {
+            Current = root;
+            yieldedStart = true;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new Node<int>(1,
+                new Node<int>(2),
+                new Node<int>(3));
+
+            var it = new InOrderIterator<int>(root);
+
+            while (it.MoveNext())
+            {
+                Console.Write(it.Current.Value);
+                Console.Write(",");
+            }
+            Console.WriteLine();
+        }
+    }
+}
