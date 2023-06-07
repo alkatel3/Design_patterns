2d0c97ffc85a107aaeacfeaaedb1fd68f838103c
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-06-07 14:36:38 +0300

    Added methods Undo and Redo

--- Memento/Memento/Memento/Program.cs
+++ Memento/Memento/Memento/Program.cs
@@ -13,16 +13,23 @@
     public class BankAccount
     {
         private int balance;
+        private List<Memento> changes
+            = new List<Memento>();
+        private int current;
 
         public BankAccount(int balance)
         {
             this.balance = balance;
+            changes.Add(new Memento(balance));
         }
 
         public Memento Deposit(int amount)
         {
             balance += amount;
-            return new Memento(balance);
+            var m = new Memento(balance);
+            changes.Add(m);
+            ++current;
+            return m;
         }
 
         public override string ToString()
@@ -32,7 +39,35 @@
 
         public void Restore(Memento m)
         {
-            balance = m.Balance;
+            if (m != null)
+            {
+
+                balance = m.Balance;
+                changes.Add(m);
+                current = changes.Count - 1; 
+            }
+        }
+
+        public Memento Undo()
+        {
+            if (current > 0)
+            {
+                var m = changes[--current];
+                balance = m.Balance;
+                return m;
+            }
+            return null;
+        }
+
+        public Memento Redo()
+        {
+            if (current + 1 < changes.Count)
+            {
+                var m = changes[++current];
+                balance = m.Balance;
+                return m;
+            }
+            return null;
         }
     }
 
@@ -46,13 +81,14 @@
             var m2 = ba.Deposit(20);
             Console.WriteLine(ba);
 
-            //m1
-            ba.Restore(m1);
-            Console.WriteLine(ba);
+            ba.Undo();
+            Console.WriteLine("Undo 1: " + ba);
 
-            //m2
-            ba.Restore(m2);
-            Console.WriteLine(ba);
+            ba.Undo();
+            Console.WriteLine("Undo 2: " + ba);
+
+            ba.Redo();
+            Console.WriteLine("Redo 1: " + ba);
         }
     }
 }
