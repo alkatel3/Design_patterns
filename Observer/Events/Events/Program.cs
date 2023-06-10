3b2d6396f6e562040b51f17c25c6faa9818745c4
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-06-10 11:13:58 +0300

    Inplemented pattern Observer as keyword event

---
+++ Observer/Events/Events/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Events
+{
+    public class FallsIllEventArgs : EventArgs
+    {
+        public string Address;
+    }
+
+    public class Person
+    {
+        public event EventHandler<FallsIllEventArgs> FallsIll;
+
+        public void CatchCold()
+        {
+            FallsIll?.Invoke(
+                this, new FallsIllEventArgs
+                {
+                    Address = "123 London Road"
+                });
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+
+            person.FallsIll += PersonFallsIll;
+
+            person.CatchCold();
+
+            person.FallsIll -= PersonFallsIll;
+        }
+
+        private static void PersonFallsIll(object? sender, FallsIllEventArgs e)
+        {
+            Console.WriteLine($"Call a doctor to {e.Address}");
+        }
+    }
+}
