285ac83e6a895bfdb7f5bcdb9623a992c55a4cd0
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2023-06-14 13:29:06 +0300

    Implemented pattern template method in functional way

---
+++ TemplateMethod/FunctionalTemplateMethod/FunctionalTemplateMethod/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace FunctionalTemplateMethod
+{
+    public class GameTemplate
+    {
+        public static void Run(
+            Action start,
+            Action takeTurn,
+            Func<bool> haveWinner,
+            Func<int> winningPlayer
+            )
+        {
+            start();
+            while (!haveWinner())
+                takeTurn();
+
+            Console.WriteLine($"Player {winningPlayer()} wins.");
+
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfPlayers = 2;
+            int currentPlayer = 0;
+            int turn = 1, maxTurns = 10;
+
+            void Start()
+            {
+                Console.WriteLine($"Starting a game of chess with {numberOfPlayers} players.");
+            }
+
+            bool HaveWinner()
+            {
+                return turn == maxTurns;
+            }
+
+            void TakeTurn()
+            {
+                Console.WriteLine($"Turn {turn++} taken by player {currentPlayer}.");
+                currentPlayer = (currentPlayer + 1) % numberOfPlayers;
+            }
+
+            int WinningPlayer()
+            {
+                return currentPlayer;
+            }
+
+            GameTemplate.Run(Start, TakeTurn, HaveWinner, WinningPlayer);
+        }
+
+
+    }
+}
